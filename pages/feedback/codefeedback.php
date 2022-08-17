2ad315f640d9471310034b13d2c3ea9a2341f014
Thulani Wickramasinghe <68929455+TM-Wickramasinghe@users.noreply.github.com>
2022-08-17 22:55:34 +0530

    Update codefeedback.php

--- pages/feedback/codefeedback.php
+++ pages/feedback/codefeedback.php
@@ -3,10 +3,10 @@ include '../includes/security.php';
 ?>
 
 <script>
-if ( window.history.replaceState ) {
-  window.history.replaceState( null, null, window.location.href );
-}
-</script> 
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href);
+    }
+</script>
 <?php
 
 //delete feedback
@@ -28,6 +28,25 @@ if (isset($_POST['deletebtn'])) {
     }
 }
 
+//delete feedback1
+
+if (isset($_POST['deletebtn1'])) {
+    $fID = $_POST['delete_fID1'];
+
+    $query = "DELETE  FROM feedback WHERE  fID='$fID' ";
+    $query_run = mysqli_query($connection, $query);
+
+    if ($query_run) {
+        $_SESSION['status'] = "Feedback Successfully DELETED";
+        $_SESSION['status_code'] = "success";
+        header('Location: feedback1.php');
+    } else {
+        $_SESSION['status'] = "Feedback is NOT DELETED";
+        $_SESSION['status_code'] = "error";
+        header('Location: feedback1.php');
+    }
+}
+
 //print report - not used
 
 if (isset($_POST['printbtn'])) {
@@ -43,7 +62,7 @@ if (isset($_POST['printbtn'])) {
         . '<h4>Comment :    ' . $msg . '</h4>';
 
     require_once __DIR__ . '/vendor/autoload.php';
-    $mpdf = new \Mpdf\Mpdf();
+    //$mpdf = new \Mpdf\Mpdf();
 
     $mpdf->WriteHTML($body);
 
