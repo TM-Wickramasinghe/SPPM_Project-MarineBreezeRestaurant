9fde0a0aa8a844f5f4e4b345e1aea4f42c2884c3
Thulani Wickramasinghe <68929455+TM-Wickramasinghe@users.noreply.github.com>
2022-08-25 05:47:06 +0530

    Update codefeedback.php

--- pages/feedback/codefeedback.php
+++ pages/feedback/codefeedback.php
@@ -10,70 +10,18 @@ include '../includes/security.php';
 <?php
 
 //delete feedback
-
-if (isset($_POST['deletebtn'])) {
-    $fID = $_POST['delete_fID'];
-
-    $query = "DELETE  FROM feedback WHERE  fID='$fID' ";
+if ($_GET['id']) {
+    $id = $_GET['id'];
+    $query = "DELETE  FROM feedback WHERE  fID='$id' ";
     $query_run = mysqli_query($connection, $query);
 
     if ($query_run) {
+        echo "Feedback deleted successfully";
         $_SESSION['status'] = "Feedback Successfully DELETED";
         $_SESSION['status_code'] = "success";
-        header('Location: feedback.php');
     } else {
+        echo "Error deleting Feedback";
         $_SESSION['status'] = "Feedback is NOT DELETED";
         $_SESSION['status_code'] = "error";
-        header('Location: feedback.php');
     }
-}
-
-//delete feedback1
-
-if (isset($_POST['deletebtn1'])) {
-    $fID = $_POST['delete_fID1'];
-
-    $query = "DELETE  FROM feedback WHERE  fID='$fID' ";
-    $query_run = mysqli_query($connection, $query);
-
-    if ($query_run) {
-        $_SESSION['status'] = "Feedback Successfully DELETED";
-        $_SESSION['status_code'] = "success";
-        header('Location: feedback1.php');
-    } else {
-        $_SESSION['status'] = "Feedback is NOT DELETED";
-        $_SESSION['status_code'] = "error";
-        header('Location: feedback1.php');
-    }
-}
-
-//print report - not used
-
-if (isset($_POST['printbtn'])) {
-    $id = $_POST['fID'];
-    $name = $_POST['fName'];
-    $email = $_POST['fEmail'];
-    $msg = $_POST['fMsg'];
-
-    $body = '<h1>EMPLOYEE DETAILS</h1>'
-        . '<h4>Customer ID :    ' . $id . '</h4>'
-        . '<h4>Full Name :    ' . $name . '</h4>'
-        . '<h4>Email :         ' . $email . '</h4>'
-        . '<h4>Comment :    ' . $msg . '</h4>';
-
-    require_once __DIR__ . '/vendor/autoload.php';
-    //$mpdf = new \Mpdf\Mpdf();
-
-    $mpdf->WriteHTML($body);
-
-    $mpdf->SetDisplayMode('fullpage');
-    $mpdf->list_indent_first_level = 0;
-
-    //call watermark content and image
-    $mpdf->SetWatermarkText('Marine Breeze');
-    $mpdf->showWatermarkText = true;
-    $mpdf->watermarkTextAlpha = 0.1;
-
-    //output in browser
-    $mpdf->Output();
 }
