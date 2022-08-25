0e57238d4bc89fbac69bf4608002ecdc2b332f5c
Thulani Wickramasinghe <68929455+TM-Wickramasinghe@users.noreply.github.com>
2022-08-25 05:52:25 +0530

    Update code.php

--- pages/reservation/code.php
+++ pages/reservation/code.php
@@ -1,6 +1,13 @@
 <?php
 include '../includes/security.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
 //add reservation 
 
 if (isset($_POST['addbtn'])) {
@@ -109,51 +116,23 @@ if (isset($_POST['update_btn1'])) {
     }
 }
 
-
-//delete reservation
-if (isset($_POST['deletebtn'])) {
-    $ID = $_POST['delete_id'];
-
-    $query = "DELETE  FROM reservation WHERE  rID='$ID' ";
+//delete reservations
+if ($_GET['id']) {
+    $id = $_GET['id'];
+    $query = "DELETE  FROM reservation WHERE  rID='$id' ";
     $query_run = mysqli_query($connection, $query);
 
     if ($query_run) {
+        echo "Record deleted successfully";
         $_SESSION['status'] = "Reservation Successfully DELETED";
         $_SESSION['status_code'] = "success";
-        header('Location: reservation.php');
     } else {
+        echo "Error deleting record";
         $_SESSION['status'] = "Reservation is NOT DELETED";
         $_SESSION['status_code'] = "error";
-        header('Location: reservation.php');
     }
 }
 
-//delete reservation1
-
-if (isset($_POST['deletebtn1'])) {
-    $ID = $_POST['delete_id1'];
-
-    $query = "DELETE  FROM reservation WHERE  rID='$ID' ";
-    $query_run = mysqli_query($connection, $query);
-
-    if ($query_run) {
-        $_SESSION['status'] = "Reservation Successfully DELETED";
-        $_SESSION['status_code'] = "success";
-        header('Location: reservation1.php');
-    } else {
-        $_SESSION['status'] = "Reservation is NOT DELETED";
-        $_SESSION['status_code'] = "error";
-        header('Location: reservation1.php');
-    }
-}
-
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
 
 //cancel reservation
 if (isset($_POST['cancelbtn'])) {
@@ -196,18 +175,9 @@ if (isset($_POST['cancelbtn'])) {
         $mail->Body    = $email_template;
         $mail->send();
 
-        $query = "DELETE  FROM reservation WHERE  rID='$ID' ";
-        $query_run = mysqli_query($connection, $query);
-
-        if ($query_run) {
-            $_SESSION['status'] = "Reservation Cancellation Successfull";
-            $_SESSION['status_code'] = "success";
-            header('Location: reservation.php');
-        } else {
-            $_SESSION['status'] = "Reservation Cancellation Failed";
-            $_SESSION['status_code'] = "error";
-            header('Location: reservation.php');
-        }
+        $_SESSION['status'] = "Reservation Cancellation Email Sent !!!";
+        $_SESSION['status_code'] = "success";
+        header('Location: reservation.php');
     } else {
         $_SESSION['status'] = "Something went wronged. #1";
         $_SESSION['status_code'] = "error";
@@ -256,18 +226,117 @@ if (isset($_POST['cancelbtn1'])) {
         $mail->Body    = $email_template;
         $mail->send();
 
-        $query = "DELETE  FROM reservation WHERE  rID='$ID' ";
-        $query_run = mysqli_query($connection, $query);
+        $_SESSION['status'] = "Reservation Cancellation Email Sent !!!";
+        $_SESSION['status_code'] = "success";
+        header('Location: reservation1.php');
+    } else {
+        $_SESSION['status'] = "Something went wronged. #1";
+        $_SESSION['status_code'] = "error";
+        header('Location: reservation1.php');
+    }
+}
 
-        if ($query_run) {
-            $_SESSION['status'] = "Reservation Cancellation Successfull";
-            $_SESSION['status_code'] = "success";
-            header('Location: reservation1.php');
-        } else {
-            $_SESSION['status'] = "Reservation Cancellation Failed";
-            $_SESSION['status_code'] = "error";
-            header('Location: reservation1.php');
-        }
+//confirm reservation
+if (isset($_POST['confirmbtn'])) {
+    $ID = $_POST['confirm_ID'];
+
+    $query1 = "SELECT * FROM reservation WHERE  rID='$ID' ";
+    $query_run1 = mysqli_query($connection, $query1);
+    $user = mysqli_fetch_assoc($query_run1);
+    $get_email = $user['rEmail'];
+
+    if ($query_run1) {
+        $mail = new PHPMailer(true);
+        //Server settings
+
+        $mail->isSMTP();                                                       //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                                 //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                               //Enable SMTP authentication
+        $mail->Username   = 'marinebreeze3@gmail.com';                         //SMTP username
+        $mail->Password   = 'uxuswdswirjdyzyh';                               //SMTP password
+        $mail->SMTPSecure = 'ssl';                                            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('marinebreeze3@gmail.com');
+        $mail->addAddress($get_email);
+
+        //Content
+        $mail->isHTML(true);
+        $mail->Subject = 'Reservation Confirmed';
+
+        $email_template = "
+            <h3>Dear Customer,</h3>
+            <h4>Thank you for making a reservation.</h4>
+            <h4>We are thrilled that you will b joining us.
+            If you would like to make special arrangement or have qustions about your reservation please do not hesitate to reach out.
+            We will hold your table for 15 minutes. So please call +94 (11) 158 5447 if you are running late.</h4>
+            <h4>W hope you enjoy your experience at Marine Breeze Restaurant.</h4>
+            <h4>Thank you.</h4>
+            <h4>Marine Breeze Restaurant,</h4>
+            <h4>Kalutara.</h4>
+            ";
+
+        $mail->Body    = $email_template;
+        $mail->send();
+
+        $_SESSION['status'] = "Confirmation Email Sent Successfully !!!";
+        $_SESSION['status_code'] = "success";
+        header('Location: reservation.php');
+    } else {
+        $_SESSION['status'] = "Something went wronged. #1";
+        $_SESSION['status_code'] = "error";
+        header('Location: reservation.php');
+    }
+}
+
+//confirm reservation1
+if (isset($_POST['confirmbtn1'])) {
+    $ID = $_POST['confirm_ID1'];
+
+    $query1 = "SELECT * FROM reservation WHERE  rID='$ID' ";
+    $query_run1 = mysqli_query($connection, $query1);
+    $user = mysqli_fetch_assoc($query_run1);
+    $get_email = $user['rEmail'];
+
+    if ($query_run1) {
+        $mail = new PHPMailer(true);
+        //Server settings
+
+        $mail->isSMTP();                                                       //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                                 //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                               //Enable SMTP authentication
+        $mail->Username   = 'marinebreeze3@gmail.com';                         //SMTP username
+        $mail->Password   = 'uxuswdswirjdyzyh';                               //SMTP password
+        $mail->SMTPSecure = 'ssl';                                            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('marinebreeze3@gmail.com');
+        $mail->addAddress($get_email);
+
+        //Content
+        $mail->isHTML(true);
+        $mail->Subject = 'Reservation Confirmed';
+
+        $email_template = "
+            <h3>Dear Customer,</h3>
+            <h4>Thank you for making a reservation.</h4>
+            <h4>We are thrilled that you will b joining us.
+            If you would like to make special arrangement or have qustions about your reservation please do not hesitate to reach out.
+            We will hold your table for 15 minutes. So please call +94 (11) 158 5447 if you are running late.</h4>
+            <h4>W hope you enjoy your experience at Marine Breeze Restaurant.</h4>
+            <h4>Thank you.</h4>
+            <h4>Marine Breeze Restaurant,</h4>
+            <h4>Kalutara.</h4>
+            ";
+
+        $mail->Body    = $email_template;
+        $mail->send();
+
+        $_SESSION['status'] = "Confirmation Email Sent Successfully !!!";
+        $_SESSION['status_code'] = "success";
+        header('Location: reservation1.php');
     } else {
         $_SESSION['status'] = "Something went wronged. #1";
         $_SESSION['status_code'] = "error";
